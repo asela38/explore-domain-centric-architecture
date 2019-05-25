789943577b16c40db658b709e4290c99234d6f10
asela <asela38@gmail.com>
2019-05-25 14:00:25 +0800

    initialization of the project

---
+++ domain-centric-application-sales/sales-specification/src/test/java/com/exploration/asela/sales/specification/SpecificationTest.java
@@ -0,0 +1,13 @@
+package com.exploration.asela.sales.specification;
+
+import org.junit.Test;
+
+public class SpecificationTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("SpecificationTest.test");
+    }
+
+}
